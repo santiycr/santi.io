5ffcede400a14970c076a5677ad26eb43d6817a2
Santiago Suarez Ordoñez <santiago@whyline.com>
2019-10-27 17:08:42 -0700

    quick and dirty link list for reaching out

--- components/ReachOut.js
+++ components/ReachOut.js
@@ -1,5 +1,8 @@
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Link from "@material-ui/core/Link";
+import { LinkedIn, Twitter, GitHub } from "@material-ui/icons";
 
 export default function ReachOut() {
   return (
@@ -7,6 +10,26 @@ export default function ReachOut() {
       <Typography variant="h5" component="h2">
         Reach Out
       </Typography>
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Grid item xs={2}>
+          <Link href="https://www.linkedin.com/in/santiycr/">
+            <LinkedIn />
+            <Typography variant="srOnly">Linkedin</Typography>
+          </Link>
+        </Grid>
+        <Grid item xs={2}>
+          <Link href="https://twitter.com/santiycr">
+            <Twitter />
+            <Typography variant="srOnly">Twitter</Typography>
+          </Link>
+        </Grid>
+        <Grid item xs={2}>
+          <Link href="https://github.com/santiycr">
+            <GitHub />
+            <Typography variant="srOnly">GitHub</Typography>
+          </Link>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
