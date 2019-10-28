7be69a824bf764929d2437bb4bdf95e724fe9169
Santiago Suarez Ordoñez <santiago@whyline.com>
2019-10-28 12:32:40 -0700

    adding more links for reaching out

--- components/ReachOut.js
+++ components/ReachOut.js
@@ -2,7 +2,7 @@ import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Link from "@material-ui/core/Link";
-import { LinkedIn, Twitter, GitHub } from "@material-ui/icons";
+import { LinkedIn, Twitter, GitHub, Email, WhatsApp } from "@material-ui/icons";
 
 export default function ReachOut() {
   return (
@@ -11,19 +11,31 @@ export default function ReachOut() {
         Reach Out
       </Typography>
       <Grid container direction="row" justify="center" alignItems="center">
-        <Grid item xs={2}>
+        <Grid item xs={1}>
+          <Link href="mailto:santiycr@gmail.com">
+            <Email />
+            <Typography variant="srOnly">Email</Typography>
+          </Link>
+        </Grid>
+        <Grid item xs={1}>
+          <Link href="https://wa.me/14158572684">
+            <WhatsApp />
+            <Typography variant="srOnly">WhatsApp</Typography>
+          </Link>
+        </Grid>
+        <Grid item xs={1}>
           <Link href="https://www.linkedin.com/in/santiycr/">
             <LinkedIn />
             <Typography variant="srOnly">Linkedin</Typography>
           </Link>
         </Grid>
-        <Grid item xs={2}>
+        <Grid item xs={1}>
           <Link href="https://twitter.com/santiycr">
             <Twitter />
             <Typography variant="srOnly">Twitter</Typography>
           </Link>
         </Grid>
-        <Grid item xs={2}>
+        <Grid item xs={1}>
           <Link href="https://github.com/santiycr">
             <GitHub />
             <Typography variant="srOnly">GitHub</Typography>
