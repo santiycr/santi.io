13392907e5b4f2cc1a5340f7e9a9b7b64757fc25
Santiago Suarez Ordoñez <santiago@whyline.com>
2019-10-30 12:50:41 -0700

    better reuse of components for isolate the experiences UI

--- components/Experience.js
+++ components/Experience.js
@@ -7,6 +7,7 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import * as colorsModule from "@material-ui/core/colors";
+import PropTypes from "prop-types";
 
 const useStyles = makeStyles(theme => ({
   pics: {
@@ -22,6 +23,138 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const experiences = [
+  {
+    image: "whyline.jpg",
+    title: "Whyline",
+    role: "CTO",
+    labels: ["entrepreneurship", "product", "leadership"],
+    description: `As the new CTO, my task is that of leading the engineering organization through
+      a new phase of growth. To take the engineering team from 40 to 100, I'm helping define core
+      engineering principles, roles and responsibilities, tools and processes for scale that meet
+      the team's culture and needs.`
+  },
+  {
+    image: "blameless.png",
+    title: "Blameless",
+    role: "Co-Founder, CTO",
+    labels: ["entrepreneurship", "product", "leadership"],
+    description: `As CTO and Co-Founder, I owned the company's team and product efforts.
+      I architected and wrote the core of the Blameless SRE Platform, which today powers the
+      incident management and reliability practices of much of our industry. Through principles and
+      key hires, I formed the company's founding engineering team and led the engineering
+      organization.`
+  },
+  {
+    image: "mulesoft.png",
+    title: "Mulesoft",
+    role: "Principal Engineer, Core Platform as a Service Team",
+    labels: ["engineering", "enterprise", "devops"],
+    description: `I was lucky enough to be at Mulesoft the company's IPO, which taught me a lot
+      about such a transformation.  Through a role of technical leader in the Core PaaS team,
+      I helped define the vision for the company's centralized infrastructure efforts and hybrid
+      cloud initiative.`
+  },
+  {
+    image: "saucelabs.jpg",
+    title: "Sauce Labs",
+    role: "Director of Engineering, Core Services",
+    labels: ["entrepreneurship", "leadership", "devops"],
+    description: `Being first employee I held many key roles in the company for over 7 years. By
+      forming and leading the Core Services team for many years, I learned extensively about
+      distributed systems, DevOps, high availability and reliability. Being responsible for such
+      a key and technical team was challenging for my leadership skills and incredibly rewarding.`
+  },
+  {
+    image: "selenium.jpg",
+    title: "The Selenium Project",
+    role: "Core Contributor",
+    labels: ["open source", "public speaking", "engineering"],
+    description: `Started contributing to Selenium back in 2004! I was lucky enough to become an
+      important member of the community, travel, speak in conferences and participate on what the
+      project and community have become today.`
+  }
+];
+
+export default function Experience() {
+  return (
+    <Container>
+      <Typography variant="h5" component="h2">
+        Experience
+      </Typography>
+      {experiences.map((experience, eid) => {
+        return <Job experience={experience} key={eid} />;
+      })}
+    </Container>
+  );
+}
+
+function Job({ experience, key }) {
+  const classes = useStyles();
+  return (
+    <Box my={2} key={key}>
+      <Card className={classes.card}>
+        <Grid container direction="row" justify="flex-start">
+          <Grid item md={2} xs={12}>
+            <CardMedia
+              className={classes.pics}
+              image={`/static/images/experience/${experience.image}`}
+              title={`${experience.title} logo`}
+            />
+          </Grid>
+          <Grid item md={10} xs={12}>
+            <CardContent>
+              <Box display="flex">
+                <Typography component="h5" variant="h5">
+                  {experience.title}
+                </Typography>
+                <br />
+                <Box flexGrow={1} />
+                {experience.labels.map((label, lid) => {
+                  return <Label label={label} key={lid} />;
+                })}
+              </Box>
+              <Typography variant="subtitle1" color="textSecondary">
+                {experience.role}
+              </Typography>
+              <p>{experience.description}</p>
+            </CardContent>
+          </Grid>
+        </Grid>
+      </Card>
+    </Box>
+  );
+}
+
+Job.propTypes = {
+  experience: PropTypes.object,
+  key: PropTypes.number
+};
+
+function Label({ label, key }) {
+  return (
+    <Box
+      component="span"
+      key={key}
+      bgcolor={getColor(label)}
+      color="white"
+      m={0.5}
+      px={0.5}
+      py={0.25}
+      borderRadius={8}
+    >
+      {label}
+    </Box>
+  );
+}
+
+Label.propTypes = {
+  label: PropTypes.string,
+  key: PropTypes.number
+};
+
+const colorAssignments = {};
+
 function randomColor() {
   const keys = Object.keys(colorsModule);
   let color = {};
@@ -31,126 +164,12 @@ function randomColor() {
   return color;
 }
 
-export default function Experience() {
-  const classes = useStyles();
-
-  const experiences = [
-    {
-      image: "whyline.jpg",
-      title: "Whyline",
-      role: "CTO",
-      labels: ["entrepreneurship", "product", "leadership"],
-      description: `As the new CTO, my task is that of leading the engineering organization through
-      a new phase of growth. To take the engineering team from 40 to 100, I'm helping define core
-      engineering principles, roles and responsibilities, tools and processes for scale that meet
-      the team's culture and needs.`
-    },
-    {
-      image: "blameless.png",
-      title: "Blameless",
-      role: "Co-Founder, CTO",
-      labels: ["entrepreneurship", "product", "leadership"],
-      description: `As CTO and Co-Founder, I owned the company's team and product efforts.
-      I architected and wrote the core of the Blameless SRE Platform, which today powers the
-      incident management and reliability practices of much of our industry. Through principles and
-      key hires, I formed the company's founding engineering team and led the engineering
-      organization.`
-    },
-    {
-      image: "mulesoft.png",
-      title: "Mulesoft",
-      role: "Principal Engineer, Core Platform as a Service Team",
-      labels: ["engineering", "enterprise", "devops"],
-      description: `I was lucky enough to be at Mulesoft the company's IPO, which taught me a lot
-      about such a transformation.  Through a role of technical leader in the Core PaaS team,
-      I helped define the vision for the company's centralized infrastructure efforts and hybrid
-      cloud initiative.`
-    },
-    {
-      image: "saucelabs.jpg",
-      title: "Sauce Labs",
-      role: "Director of Engineering, Core Services",
-      labels: ["entrepreneurship", "leadership", "devops"],
-      description: `Being first employee I held many key roles in the company for over 7 years. By
-      forming and leading the Core Services team for many years, I learned extensively about
-      distributed systems, DevOps, high availability and reliability. Being responsible for such
-      a key and technical team was challenging for my leadership skills and incredibly rewarding.`
-    },
-    {
-      image: "selenium.jpg",
-      title: "The Selenium Project",
-      role: "Core Contributor",
-      labels: ["open source", "public speaking", "engineering"],
-      description: `Started contributing to Selenium back in 2004! I was lucky enough to become an
-      important member of the community, travel, speak in conferences and participate on what the
-      project and community have become today.`
-    }
-  ];
-
-  const colorAssignments = {};
-
-  function getColor(label) {
-    if (label in colorAssignments) {
-      return colorAssignments[label];
-    } else {
-      const color = randomColor();
-      colorAssignments[label] = color[500];
-      return color;
-    }
+function getColor(label) {
+  if (label in colorAssignments) {
+    return colorAssignments[label];
+  } else {
+    const color = randomColor();
+    colorAssignments[label] = color[500];
+    return color;
   }
-
-  return (
-    <Container>
-      <Typography variant="h5" component="h2">
-        Experience
-      </Typography>
-      {experiences.map((experience, eid) => {
-        return (
-          <Box my={2} key={eid}>
-            <Card className={classes.card}>
-              <Grid container direction="row" justify="flex-start">
-                <Grid item xs={2}>
-                  <CardMedia
-                    className={classes.pics}
-                    image={`/static/images/experience/${experience.image}`}
-                    title={`${experience.title} logo`}
-                  />
-                </Grid>
-                <Grid item xs={10}>
-                  <CardContent>
-                    <Box display="flex">
-                      <Typography component="h5" variant="h5">
-                        {experience.title}
-                      </Typography>
-                      <Box flexGrow={1} />
-                      {experience.labels.map((label, lid) => {
-                        return (
-                          <Box
-                            component="span"
-                            key={lid}
-                            bgcolor={getColor(label)}
-                            color="white"
-                            m={0.5}
-                            px={0.5}
-                            py={0.25}
-                            borderRadius={8}
-                          >
-                            {label}
-                          </Box>
-                        );
-                      })}
-                    </Box>
-                    <Typography variant="subtitle1" color="textSecondary">
-                      {experience.role}
-                    </Typography>
-                    <p>{experience.description}</p>
-                  </CardContent>
-                </Grid>
-              </Grid>
-            </Card>
-          </Box>
-        );
-      })}
-    </Container>
-  );
 }
