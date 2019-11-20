4b061d4b140b5c92bbb0eb608c0e655aa3d498d3
Santiago Suarez Ordoñez <santiago@whyline.com>
2019-11-20 11:46:26 -0800

    tweaking skills section, not sure how valuable this is

--- components/Skills.js
+++ components/Skills.js
@@ -5,31 +5,26 @@ import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Heading from "./Heading";
 
 const data = [
-  { text: "Leadership", value: 100 },
-  { text: "Engineering", value: 100 },
-  { text: "Entrepreneurship", value: 80 },
-  { text: "Startups", value: 80 },
-  { text: "Enterprise", value: 20 },
-  { text: "Architecture", value: 20 },
-  { text: "Distributed Systems", value: 50 },
-  { text: "Python", value: 60 },
-  { text: "CI/CD", value: 60 },
-  { text: "AWS", value: 60 },
-  { text: "Test Automation", value: 60 },
-  { text: "Open Source", value: 60 },
-  { text: "Infrastructure", value: 50 },
-  { text: "DevOps", value: 50 },
-  { text: "Nameko", value: 30 },
-  { text: "Kubernetes", value: 30 },
-  { text: "Javascript", value: 20 },
-  { text: "React", value: 10 },
+  { text: "Leadership", value: 40 },
+  { text: "Engineering", value: 40 },
+  { text: "Product Market Fit", value: 40 },
+  { text: "Startups", value: 30 },
+  { text: "Architecture", value: 30 },
+  { text: "Distributed Systems", value: 30 },
+  { text: "Python", value: 30 },
+  { text: "CI/CD", value: 20 },
+  { text: "AWS", value: 30 },
+  { text: "Open Source", value: 30 },
+  { text: "Infrastructure", value: 30 },
+  { text: "DevOps", value: 30 },
+  { text: "Test Automation", value: 20 },
+  { text: "Kubernetes", value: 15 },
+  { text: "Javascript", value: 10 },
   { text: "Nodejs", value: 10 },
   { text: "HTML5", value: 10 },
-  { text: "CSS", value: 5 },
-  { text: "MongoDB", value: 20 }
+  { text: "CSS", value: 10 },
+  { text: "MongoDB", value: 10 }
 ];
-
-const fontSizeMapper = word => Math.log2(word.value ^ 2) * 6;
 
 const SimpleCloud = () => {
   const theme = useTheme();
@@ -38,7 +33,6 @@ const SimpleCloud = () => {
   return (
     <WordCloud
       data={data}
-      fontSizeMapper={fontSizeMapper}
       padding={3}
       height={matches ? 300 : 600}
       width={matches ? 900 : 300}
